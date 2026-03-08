31e0f3fab5f08c404427c3d09747ee73acc78de1
yasindugunasekara <yasinduramrasu2020@gmail.com>
2026-03-08 16:15:00 +0530

    Add console log for server start confirmation

--- EC2 (day-1)/backend/index.js
+++ EC2 (day-1)/backend/index.js
@@ -15,5 +15,6 @@ app.use("/notes", notesRouter);
 // Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
+  console.log(`done`);
   console.log(`Server running on http://localhost:${PORT}`);
 });
