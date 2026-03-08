a3a89854addba5e25a0acb1f56f8369f9f6739aa
yasindugunasekara <yasinduramrasu2020@gmail.com>
2026-03-08 16:17:29 +0530

    Change log message from 'done' to 'running'

--- EC2 (day-1)/backend/index.js
+++ EC2 (day-1)/backend/index.js
@@ -15,6 +15,6 @@ app.use("/notes", notesRouter);
 // Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`done`);
+  console.log(`running `);
   console.log(`Server running on http://localhost:${PORT}`);
 });
