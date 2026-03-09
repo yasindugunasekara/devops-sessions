e449c7cb8c305914ee9d00bcc4d7cbe46f4202d3
yasindugunasekara <yasinduramrasu2020@gmail.com>
2026-03-09 07:50:39 +0530

    Fix typo in server start log message

--- EC2 (day-1)/backend/index.js
+++ EC2 (day-1)/backend/index.js
@@ -15,6 +15,6 @@ app.use("/notes", notesRouter);
 // Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`server is now running noww  `);
+  console.log(`server is now running no  `);
   console.log(`Server running on http://localhost:${PORT}`);
 });
